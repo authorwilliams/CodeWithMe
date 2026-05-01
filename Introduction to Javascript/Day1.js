0cbfa88a8d14dd00b072612a37a8a9e6c5b725d6
authorwilliams <williampaulbitrus@gmai.com>
2026-05-01 15:53:38 +0100

    Day 1

--- Introduction to Javascript/Day1.js
+++ Introduction to Javascript/Day1.js
@@ -3,13 +3,75 @@
  * ================== Day 1 =================
  * 
  * 
- * todo: Intro
- * todo: Syntax & Types
- * todo: Operators
- * todo: Decision
+ * done: Intro
+ * done: Syntax & Types
+ * done: Operators
+ * done: Decision
  * 
  * _________________________________________
  * ===============CodeWithMe================
 */  
 
 
+const person = {
+    firstName: 'Author',
+    lastName: 'Williams',
+    bloodType: 'AA',
+    age: 17,
+    nationality: 'Nigeria',
+    favoriteColor: 'Black',
+}
+
+
+const players = ['Mayor', true, 10, person]
+
+
+let firstName = 'Author'
+
+
+// ---------------
+
+
+
+let num1 = 10
+let num2 = 10
+
+// console.log(num1 != num2)
+
+
+// ======= Decision =======
+
+let minimumAge = 18
+
+// if(person.age === minimumAge) {
+//     console.log('Old')
+// } else if (person.age > minimumAge) {
+//     console.log('Old enough')
+// } else {
+//     console.log('Too young')
+// }
+
+let result;
+
+// if(10 > 9) {
+//     result = 'Yeah'
+// } else {
+//     result = 'No'
+// }
+
+// result = 10 > 9 ? 'Yeah' : 'No'
+
+
+console.log(10 < 9 || 10 == 11)
+
+// switch (person.age) {
+//     case 17:
+//             console.log('Yeah got it')
+//         // break;
+//         break;
+
+//     default:
+//         console.log('Condition not true')
+//         break;
+// }
+// console.log(result)
